46a52bb1aefd46c208c2af3f23cb2653c3a1995a
Ender KUŞ <enderkus@msn.com>
2018-11-29 12:13:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
-
+// Limonad frameworkü projemize dahil ettik.
 require_once 'vendors/limonade.php';
-
+  
+ // Ana dizin yani index sayfamız için tanımlama gerçekleştirdik. 
   dispatch('/', 'hello');
   function hello()
   {
+      // Projemizin ana sayfasında "Hello world!" yazıyor olacak.
       return 'Hello world!';
   }
   
-  
+  // Projemiz için yeni bir sayfa tanımlaması yaptık /merhaba. Ve parametre tanımladık merhaba/Ender yazarsak ekrana Merhaba Ender yazacak.
   dispatch('/merhaba/:isim', 'merhaba');
   function merhaba()
   {
